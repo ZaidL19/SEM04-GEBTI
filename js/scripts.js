07ebb340f14ccdb1b37036112e62981de47be232
OlortigueSanderExe <turnertimi15xd@gmail.com>
2026-05-18 00:26:37 -0500

    V10.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,49 @@
 history
 var nombre = "Johan Lopez"
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+/fORMULARIO/
+const form = document.getElementById("appointment-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    alert("Solicitud enviada correctamente ✅");
+
+    form.reset();
+});
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+
+        const increment = target / 100;
+
+        if (current < target) {
+            counter.innerText = `${Math.ceil(current + increment)}`;
+            setTimeout(updateCounter, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
+
+const toggleButton = document.getElementById("dark-mode-toggle");
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
